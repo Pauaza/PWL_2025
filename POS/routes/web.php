e3b08fcaebaa499a9cb43f6f36a81d41c62230ef
Pauaza <paudrabuana@gmail.com>
2025-04-14 20:11:58 +0700

    Update_JS5.1_to_POS

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -1,26 +1,55 @@
 <?php
-
-// Import class Route dari Laravel untuk mendefinisikan routing
-use Illuminate\Support\Facades\Route;
-// Import ItemController untuk menghubungkan route dengan controller
-use App\Http\Controllers\ItemController;
-use App\Http\Controllers\LevelController;
+use App\Http\Controllers\BarangController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Route;
 
-// Route untuk halaman utama ('/') yang menampilkan view welcome
-Route::get('/', function () {
-    return view('welcome'); // Tampilkan halaman welcome
+Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);          // menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);      // menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']);   // menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);        // menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);      // menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);    // menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']);// menghapus data user
 });
+    //level route controller
+    Route::group(['prefix' => 'level'], function () {
+        Route::get('/', [LevelController::class, 'index']);       
+        Route::post('/list', [LevelController::class, 'list']);    
+        Route::get('/create', [LevelController::class, 'create']); 
+        Route::post('/', [LevelController::class, 'store']);   
+        Route::get('/{id}', [LevelController::class, 'show']);     
+        Route::get('/{id}/edit', [LevelController::class, 'edit']);
+        Route::put('/{id}', [LevelController::class, 'update']);   
+        Route::delete('/{id}', [LevelController::class, 'destroy']);
+    });
+    
+    //kategori route controller
+    Route::group(['prefix' => 'kategori'], function () {
+        Route::get('/', [KategoriController::class, 'index']);       
+        Route::post('/list', [KategoriController::class, 'list']);    
+        Route::get('/create', [KategoriController::class, 'create']); 
+        Route::post('/', [KategoriController::class, 'store']);   
+        Route::get('/{id}', [KategoriController::class, 'show']);     
+        Route::get('/{id}/edit', [KategoriController::class, 'edit']);
+        Route::put('/{id}', [KategoriController::class, 'update']);   
+        Route::delete('/{id}', [KategoriController::class, 'destroy']);
+    });
+    //Data route controller
+    Route::group(['prefix' => 'barang'], function () {
+        Route::get('/', [BarangController::class, 'index']);       
+        Route::post('/list', [BarangController::class, 'list']);    
+        Route::get('/create', [BarangController::class, 'create']); 
+        Route::post('/', [BarangController::class, 'store']);   
+        Route::get('/{id}', [BarangController::class, 'show']);     
+        Route::get('/{id}/edit', [BarangController::class, 'edit']);
+        Route::put('/{id}', [BarangController::class, 'update']);   
+        Route::delete('/{id}', [BarangController::class, 'destroy']);
+    });
 
-Route::get('user/hapus/{id}', [UserController::class, 'hapus']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::get('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::post('user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-// Route resource untuk ItemController
-// Ini otomatis membuat route untuk CRUD (Create, Read, Update, Delete) item
-Route::resource('items', ItemController::class);
