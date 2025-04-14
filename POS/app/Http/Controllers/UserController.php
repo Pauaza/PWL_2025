184c5d5f26bfbd82b16728fd1bd951ad036c1cef
Pauaza <paudrabuana@gmail.com>
2025-04-14 20:07:11 +0700

    Update_JS4_to_POS

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -9,16 +9,67 @@ class UserController extends Controller
 {
     public function index()
     {
-        //tambah data user dengan eloquent model
-        $data = [
 
-            'nama' => 'Pelanggan Pertama',
+        $user = UserModel::with('level')->get();
+        //dd($user);
+        return view('user', ['data' => $user]);
+    //     $user = UserModel::create([
+    //         'username' => 'manager11',
+    //         'nama' => 'Manager11',
+    //         'password' => Hash::make('12345'),
+    //         'level_id' => 2,
+    //     ]);
 
-        ];
-        UserModel::where('username', 'customer-1')->update($data); //update data user
+    //     $user->username = 'manager12';
 
-        //coba akses model UserModel
-        $user = UserModel::all(); //ambil data dari tabel m_user
-        return view('user',['data' => $user]);
-    }
+    //     $user->save();
+
+    //     $user->wasChanged();
+    //     $user->wasChanged('useername');
+    //     $user->wasChanged(['username', 'level_id']);
+    //     $user->wasChanged('nama');
+    //     dd($user->wasChange(['nama', 'username']));
+     }
+     public function tambah()
+     {
+        return view('user_tambah');
+     }
+
+     public function tambah_simpan(Request $request)
+     {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make('$request->password'),
+            'level_id' => $request->level_id
+        ]);
+     }
+
+     public function ubah($id)
+     {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+     }
+
+     public function ubah_simpan($id, Request $request)
+     {
+        $user = UserModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make('$request->password');
+        $user->level_id = $request->level_id;
+
+        $user->save();
+
+        return redirect('/user');
+     }
+
+     public function hapus($id)
+     {
+        $user = UserModel::find($id);
+        $user->delete();
+
+        return redirect('/user');
+     }
 }
--- POS/routes/web.php
+++ POS/routes/web.php
@@ -13,6 +13,11 @@ Route::get('/', function () {
     return view('welcome'); // Tampilkan halaman welcome
 });
 
+Route::get('user/hapus/{id}', [UserController::class, 'hapus']);
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::get('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+Route::post('user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+Route::get('/user/tambah', [UserController::class, 'tambah']);
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
